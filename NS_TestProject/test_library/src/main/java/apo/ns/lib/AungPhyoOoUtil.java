048db9b11fc168e231e7139b241650dc67fa2fa5
NSAungPhyoOo <aungphyoe639@gmail.com>
2020-12-18 08:30:27 +0630

    COMMIT

---
+++ NS_TestProject/test_library/src/main/java/apo/ns/lib/AungPhyoOoUtil.java
@@ -0,0 +1,11 @@
+package apo.ns.lib;
+import android.content.*;
+import android.widget.*;
+
+public class AungPhyoOoUtil
+{
+	public static void ToastShow(Context context , String s){
+	Toast.makeText(context,s,Toast.LENGTH_SHORT).show();
+	
+	}
+}
